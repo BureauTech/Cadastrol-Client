7927a1fc4bd068f96547efacc9b5bdcdd813be08
Wesley Dias <wesd_444@hotmail.com>
2022-03-15 15:46:10 -0300

    Adição estrutura base do projeto

---
+++ src/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app class="d-flex">
+    Base project
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "App"
+  }
+</script>
+
+<style>
+  #app {
+    background: #E5E5E5;
+    color: #121214;
+  }
+
+  .toasted-font {
+    font-family: "Roboto", sans-serif;
+  }
+
+  .v-card__text, .v-card__title {
+    word-break: normal !important;
+  }
+</style>
