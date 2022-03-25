5e37b438eb7c6ee748199953f980c3a3bd385e3c
charles-ramos <charles.ramos@fatec.sp.gov.br>
2022-03-25 19:05:25 -0300

    ajustes de layout de formulários

--- src/src/App.vue
+++ src/src/App.vue
@@ -11,11 +11,7 @@ export default {
 </script>
 <style>
 #app {
-	background: linear-gradient(
-		359.76deg,
-		rgba(42, 100, 132, 0.8) 0.23%,
-		rgba(196, 196, 196, 0) 99.82%
-	);
+	background-color: #f3f3f3
 }
 .toasted-font {
 	font-family: "Roboto", sans-serif;
--- src/src/views/EditUser/EditUser.vue
+++ src/src/views/EditUser/EditUser.vue
@@ -1,131 +1,131 @@
 <template>
-  <v-app>
-    <v-progress-linear
-      :active="loading"
-      color="deep-purple"
-      height="10"
-      indeterminate
-    ></v-progress-linear>
-    <v-form>
-      <v-layout
-        row
-        justify-center
-        margin
-        align-top
-        class="no-negative"
-      >
-        <template slot="progress">
-          <v-progress-linear
-            color="deep-purple"
-            height="10"
-            indeterminate
-          ></v-progress-linear>
-        </template>
-        <v-row
-          class="mt-5 justify-space-around align-start"
-          no-gutters
-        >
-          <v-col
-            sm="8"
-            md="6"
-            cols="11"
-          >
-            <p class="text-h4 bahama--text pb-10">Editar usuário</p>
-            <v-row class="justify-center">
-              <v-col md="3">
-                <b>Nome:</b>
-              </v-col>
-              <v-text-field
-                rounded
-                outlined
-                :rules="[rules.required]"
-                v-model="user.useName"
-              />
-            </v-row>
-
-            <v-row class="justify-center">
-              <v-col md="3">
-                <b>Email:</b>
-              </v-col>
-              <v-text-field
-                rounded
-                outlined
-                type="email"
-                :rules="[rules.required, rules.email]"
-                v-model="user.useEmail"
-              />
-            </v-row>
-            <v-row class="justify-center">
-              <v-col md="3">
-                <b>Telefone:</b>
-              </v-col>
-              <v-text-field
-                type="tel"
-                rounded
-                outlined
-                :rules="[rules.number]"
-                v-model="user.usePhone"
-              />
-            </v-row>
-            <v-row class="justify-center">
-              <v-col md="3">
-                <b>Senha:</b>
-              </v-col>
-              <v-text-field
-                rounded
-                outlined
-                :rules="[rules.required]"
-                v-model="user.usePassword"
-                @click:append="() => (value = !value)"
-                :type="value ? 'text' : 'password'"
-                :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
-              />
-            </v-row>
-            <v-row class="justify-center">
-              <v-col md="3">
-                <b>Confirmar Senha:</b>
-              </v-col>
-              <v-text-field
-                rounded
-                outlined
-                type="password"
-                :rules="[rules.required, rules.same(samePass, user.usePassword)]"
-                v-model="samePass"
-              />
-            </v-row>
-            <v-row class="mt-5 justify-space-around align-start">
-
-              <v-col md="5">
-                <v-btn
-                  min-width="100%"
-                  elevation="2"
-                  rounded
-                  color="#2A6484"
-                  class="white--text text-none text-h5"
-                  @click="editUser"
-                >
-                  Salvar
-                </v-btn>
-              </v-col>
-
-              <v-col md="5">
-                <v-btn
-                  min-width="100%"
-                  elevation="2"
-                  rounded
-                  color="red"
-                  class="white--text text-none text-h5"
-                  @click="cancel"
-                >
-                  Cancelar
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-layout>
-    </v-form>
-  </v-app>
+	<v-app>
+		<v-progress-linear
+			:active="loading"
+			color="deep-purple"
+			height="10"
+			indeterminate
+		></v-progress-linear>
+		<v-form>
+			<v-layout
+				row
+				justify-center
+				margin
+				align-top
+				class="no-negative"
+			>
+				<template slot="progress">
+					<v-progress-linear
+						color="deep-purple"
+						height="10"
+						indeterminate
+					></v-progress-linear>
+				</template>
+				<v-row
+					class="mt-5 justify-space-around align-start"
+					no-gutters
+				>
+					<v-col
+						sm="8"
+						md="6"
+						cols="11"
+					>
+						<p class="text-h4 bahama--text pb-10">Editar usuário</p>
+						<v-row class="justify-center">
+							<v-col md="3">
+								<b>Nome:</b>
+							</v-col>
+							<v-text-field
+								rounded
+								outlined
+								:rules="[rules.required]"
+								v-model="user.useName"
+							/>
+						</v-row>
+						<v-row class="justify-center">
+							<v-col md="3">
+								<b>Email:</b>
+							</v-col>
+							<v-text-field
+								rounded
+								outlined
+								type="email"
+								:rules="[rules.required, rules.email]"
+								v-model="user.useEmail"
+							/>
+						</v-row>
+						<v-row class="justify-center">
+							<v-col md="3">
+								<b>Telefone:</b>
+							</v-col>
+							<v-text-field
+								type="tel"
+								rounded
+								outlined
+								:rules="[rules.number]"
+								v-model="user.usePhone"
+							/>
+						</v-row>
+						<v-row class="justify-center">
+							<v-col md="3">
+								<b>Senha:</b>
+							</v-col>
+							<v-text-field
+								rounded
+								outlined
+								:rules="[rules.required]"
+								v-model="user.usePassword"
+								@click:append="() => (value = !value)"
+								:type="value ? 'text' : 'password'"
+								:append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+							/>
+						</v-row>
+						<v-row class="justify-center">
+							<v-col md="3">
+								<b>Confirmar Senha:</b>
+							</v-col>
+							<v-text-field
+								rounded
+								outlined
+								type="password"
+								:rules="[rules.required, rules.same(samePass, user.usePassword)]"
+								v-model="samePass"
+								@click:append="() => (value = !value)"
+								:type="value ? 'text' : 'password'"
+								:append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+							/>
+						</v-row>
+						<v-row class="mt-5 justify-space-around align-start">
+							<v-col md="5">
+								<v-btn
+									min-width="100%"
+									elevation="2"
+									rounded
+									color="red"
+									class="white--text text-none text-h5"
+									@click="cancel"
+								>
+									Cancelar
+								</v-btn>
+							</v-col>
+								<v-col md="5">
+								<v-btn
+									min-width="100%"
+									elevation="2"
+									rounded
+									color="#2A6484"
+									class="white--text text-none text-h5"
+									@click="editUser"
+								>
+									Salvar
+								</v-btn>
+							</v-col>
+						</v-row>
+					</v-col>
+				</v-row>
+			</v-layout>
+		</v-form>
+	</v-app>
 </template>
 
 <script src="./EditUser.js"/>
